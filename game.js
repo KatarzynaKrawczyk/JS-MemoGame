cdbf797de1de763de3315dd3d277ac06a1bfae85
Katarzyna Krawczyk <krawczyk.kat@gmail.com>
2019-04-09 00:48:57 +0200

    HTMLCollection to Array messed but fixed

--- game.js
+++ game.js
@@ -1,17 +1,28 @@
+
 const CardsColor = [
     "red", "blue", "aqua", "gold", "gray", "black", 
     "red", "blue", "aqua", "gold", "gray", "black"
 ];
-let cards = document.getElementsByClassName("card-body");
+
+let cards2 = document.querySelectorAll("div");
+console.log(cards2);
+cards2 = [...cards2];
+console.log(cards2);
+console.log(cards2.length);
+
+//let divCards = document.querySelectorAll("div");
+//console.log(divCards);
+
+let cards = document.getElementsByClassName('card-body');
 console.log(cards);
 console.log(cards instanceof Array);
 
+
+
+console.log(cards);
 cards = [...cards];
+console.log(cards);
 console.log(cards.length);
-console.log(cards instanceof Array);
-console.log(cards[3]);
-
-
 //change to table
 
 
