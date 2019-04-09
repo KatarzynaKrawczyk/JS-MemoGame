3ce87274de05bef5617f5280449b1672ac12e9cc
Katarzyna Krawczyk <krawczyk.kat@gmail.com>
2019-04-09 20:26:56 +0200

    class hidden

--- game.js
+++ game.js
@@ -4,33 +4,27 @@ const CardsColor = [
     "red", "blue", "aqua", "gold", "gray", "black"
 ];
 
-let cards2 = document.querySelectorAll("div");
-console.log(cards2);
-cards2 = [...cards2];
-console.log(cards2);
-console.log(cards2.length);
-
-//let divCards = document.querySelectorAll("div");
-//console.log(divCards);
 
 let cards = document.getElementsByClassName('card-body');
-console.log(cards);
-console.log(cards instanceof Array);
-
-
-
-console.log(cards);
 cards = [...cards];
 console.log(cards);
 console.log(cards.length);
 //change to table
 
-
-
-const init = function(){
+const init = function() {
     cards.forEach(function(card) {
         const position = Math.floor(Math.random() * CardsColor.length);
         card.classList.add(CardsColor[position]);
+        CardsColor.splice(position, 1);
     } )
+
+    setTimeout(function () {
+        cards.forEach(function (card){
+            card.classList.add('hidden')
+        })        
+    }, 2000)
 };
-//init();
+
+init();
+
+
